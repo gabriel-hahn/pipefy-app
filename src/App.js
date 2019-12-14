2c1c6a5cc19375ebb182f61377c84a140030fccd
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-12-14 11:41:46 +0100

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div>
-      <p>React application</p>
-    </div>
+    <GlobalStyle />
   );
 }
 
