117e2bb0ad05d6b9ed9c50bd7df8c5513d918bb9
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-12-18 21:10:36 +0100

    Initial style finished

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -5,22 +5,20 @@ import { Container } from './styles';
 
 import Card from '../Card';
 
-export default function List() {
+export default function List({ data }) {
   return (
-    <Container>
+    <Container done={data.done}>
       <header>
-        <h2>Tasks</h2>
-        <button type="button">
-          <MdAdd size={24} color="#fff" />
-        </button>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#fff" />
+          </button>
+        )}
       </header>
 
       <ul>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data.cards.map(card => <Card key={card.id} data={card} />)}
       </ul>
     </Container>
   );
